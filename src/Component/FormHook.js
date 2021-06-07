4675a0e4ae95bf8cd97714b403f8e7685a1bc621
qixiaoling <x.qi@novi-education.nl>
2021-06-07 12:07:23 +0200

    form onSubmit button

--- src/Component/FormHook.js
+++ src/Component/FormHook.js
@@ -10,110 +10,108 @@ function FormHook() {
         }
     })
 
-    function onFormSubmit(e) {
-        e.preventDefault();
-        console.log('Submitted');
+    function onFormSubmit(data) {
+
+        console.log(data)
     }
 
-    return(
-        <div className='form-container'>
-            <div>
-                <form className='form-container-inside' onSubmit={handleSubmit(onFormSubmit)}>
-                    <label htmlFor='first-name'>
-                        <input
-                            type='text'
-                            id='first-name'
-                            {...register("firstName")}
-                        />
-                    </label>
-                    <label htmlFor='last-name'>
-                        <input
-                            type='text'
-                            id='last-name'
-                            {...register("lastName")}
-                        />
-                    </label>
-                    <label htmlFor='age'>
-                        <input
-                            type='number'
-                            id='age'
-                            {...register("age")}
-                        />
-                    </label>
-                    <label htmlFor='postcode'>
-                        <input
-                            type='text'
-                            id='postcode'
-                            {...register("postCode")}
-                        />
-                    </label>
-                </form>
-                <div className="radio-container">
-                    <p>Bezorgfrequentie</p>
-                    <label htmlFor='frequency'>
-                        <input
-                            type="radio"
-                            id="frequency"
-                            value='Iedere Week'
-                            {...register("frequency")}
-                        />Iedere week
-                    </label>
+    return (
 
-                    <label htmlFor='frequency'>
-                        <input
-                            type="radio"
-                            id="frequency"
-                            value='Om de week'
-                            {...register("frequency")}
-                        />Om de week
-                    </label>
+        <form className='form-container-inside' onSubmit={handleSubmit(onFormSubmit)}>
+            <label htmlFor='first-name'>
+                <input
+                    type='text'
+                    id='first-name'
+                    {...register("firstName")}
+                />
+            </label>
+            <label htmlFor='last-name'>
+                <input
+                    type='text'
+                    id='last-name'
+                    {...register("lastName")}
+                />
+            </label>
+            <label htmlFor='age'>
+                <input
+                    type='number'
+                    id='age'
+                    {...register("age")}
+                />
+            </label>
+            <label htmlFor='postcode'>
+                <input
+                    type='text'
+                    id='postcode'
+                    {...register("postCode")}
+                />
+            </label>
 
-                    <label htmlFor='frequency'>
-                        <input
-                            type="radio"
-                            id="frequency"
-                            value='Iedere maand'
-                            {...register("frequency")}
-                        />Iedere maand
-                    </label>
+            <div className="radio-container">
+                <p>Bezorgfrequentie</p>
+                <label htmlFor='frequency'>
+                    <input
+                        type="radio"
+                        id="frequency"
+                        value='Iedere Week'
+                        {...register("frequency")}
+                    />Iedere week
+                </label>
 
-                    <label htmlFor='frequency'>
-                        <input
-                            type="radio"
-                            id="frequency"
-                            value='Anders'
-                            {...register("frequency")}
-                        />Anders
-                    </label>
+                <label htmlFor='frequency'>
+                    <input
+                        type="radio"
+                        id="frequency"
+                        value='Om de week'
+                        {...register("frequency")}
+                    />Om de week
+                </label>
 
-                </div>
-                <div className='review-container'>
-                    <label htmlFor='review'>
-                        Opmerkingen:
-                        <textarea
-                            id='review'
-                            rows='30'
-                            cols='20'
-                            placeholder='Wat vond je?'
-                            {...register("review")}>
-                        </textarea>
-                    </label>
-                </div>
-                <div className="agreement">
-                    <label htmlFor='agreement'>
-                        <input
-                            type='checkbox'
-                            id='agreement'
-                            {...register("agreement")}
-                        />
-                        Ik ga akkoord met de voorwaarden
-                    < /label>
-                </div>
-                <button type='submit'>
-                    Verzend
-                </button>
+                <label htmlFor='frequency'>
+                    <input
+                        type="radio"
+                        id="frequency"
+                        value='Iedere maand'
+                        {...register("frequency")}
+                    />Iedere maand
+                </label>
+
+                <label htmlFor='frequency'>
+                    <input
+                        type="radio"
+                        id="frequency"
+                        value='Anders'
+                        {...register("frequency")}
+                    />Anders
+                </label>
+
             </div>
-        </div>
+            <div className='review-container'>
+                <label htmlFor='review'>
+                    Opmerkingen:
+                    <textarea
+                        id='review'
+                        rows='30'
+                        cols='20'
+                        placeholder='Wat vond je?'
+                        {...register("review")}>
+                        </textarea>
+                </label>
+            </div>
+            <div className="agreement">
+                <label htmlFor='agreement'>
+                    <input
+                        type='checkbox'
+                        id='agreement'
+                        {...register("agreement")}
+                    />
+                    Ik ga akkoord met de voorwaarden
+                < /label>
+            </div>
+            <button type='submit'>
+                Verzend
+            </button>
+        </form>
     )
 }
 
